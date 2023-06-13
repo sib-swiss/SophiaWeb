c5e96eef572ad6e1fafe43f9028da5bad1e9eeb3
Iulian Dragan <iulian_dragan@yahoo.com>
2023-06-13 09:57:10 +0200

    MyCORSMw

--- src/app.R
+++ src/app.R
@@ -2,8 +2,49 @@ library(jsonlite)
 library(RestRserve)
 library(magrittr)
 
+MyCORSMw <- R6::R6Class(
+  classname = "MyCORSMw",
+  inherit = CORSMiddleware, 
+  public = list(
+    initialize = function(routes = "/", match = "partial", id = "CORSMiddleware") {
+      checkmate::assert_character(routes, pattern = "^/")
+      checkmate::assert_subset(match, c("exact", "partial"))
+      checkmate::assert_string(id, min.chars = 1L)
+      
+      if (length(match) == 1L) {
+        match = rep(match, length(routes))
+      }
+      if (length(routes) != length(match)) {
+        stop("length 'match' must be 1 or equal length 'routes'")
+      }
+      
+      self$id = id
+      
+      self$process_response = function(request, response) {
+        prefixes_mask = match == "partial"
+        if ((request$path %in% routes[!prefixes_mask]) ||
+            any(startsWith(request$path, routes[prefixes_mask])) &&
+            # response successful
+            response$status_code < 300) {
+          response$set_header("Access-Control-Allow-Origin", response$get_header("Access-Control-Allow-Origin", "*"))
+          response$set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+          response$set_header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Request-With")
+        }
+        invisible(TRUE)
+      }
+      
+      self$process_request = function(request, response) {
+        invisible(TRUE)
+      }
+    }
+  )
+    
+)
+
+
+
 loadApp <- function(){
-  app <-  Application$new(middleware = list(CORSMiddleware$new()), content_type = "application/json")
+  app <-  Application$new(middleware = list(MyCORSMw$new()), content_type = "application/json")
   #app <-  Application$new( content_type = "application/json")
   
   # load the enpoint descriptions in a sandbox env:
@@ -33,5 +74,14 @@ config <- readChar(confFile, file.info(confFile)$size) %>%
 ##### load and start the app:
 app <- loadApp()
 
+################## debug ##############
+#req = Request$new(
+#  path = "/getvars",
+#  headers = list("Access-Control-Request-Method" = "GET"),
+#  method = "GET"
+#)
+
+# x <- app$process_request(req)
+######################################
 backend <- BackendRserve$new()
 backend$start(app, http_port = config$port)
