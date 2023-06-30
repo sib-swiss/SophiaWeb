c2df21278c26136ce78ef99e2ea2ac0eafdf7a59
Iulian Dragan <iulian_dragan@yahoo.com>
2023-06-30 13:12:29 +0200

    error handling + more data

--- src/updateCache.R
+++ src/updateCache.R
@@ -8,13 +8,17 @@ setwd('/mnt/shareddisk/datashield/SophiaWeb/src')
 source('appLogin.R')
 source('prepareData.R')
 source('oneVarStats.R')
-confFile <- './config.json'
+confFile <- '../config/config.connect'
 config <- readChar(confFile, file.info(confFile)$size) %>%
   gsub('(?<=:)\\s+|(?<=\\{)\\s+|(?<=,)\\s+|(?<=\\[)\\s+','',., perl = TRUE) %>%
   fromJSON()
-opals <- appLogin(config, '3xC@libur')
-varList <- prepareData(opals)
-write(toJSON(varList),'data/vars.json')
+source('/mnt/shareddisk/Sophia/super_secret.R')
+opals <- appLogin(config,  getOption('datashield.password'))
+varList <-tryCatch(prepareData(opals), error = function(e){
+  datashield.logout(opals)
+  stop(e)
+})
+write(toJSON(varList),'../data/vars.json')
 
 datashield.symbols(opals)
 
@@ -22,17 +26,21 @@ datashield.symbols(opals)
 sts <- sapply(names(varmap), function(x){
   if(varmap[[x]]$type == 'number'){
     sapply(varmap[[x]]$cohorts, function(y){
-      oneVarStats(x,opals[y])
+      tryCatch(oneVarStats(x,opals[y]), error = function(e){
+        datashield.errors()
+        datashield.logout(opals[y])
+      })
     }, simplify = FALSE)
   }
 }, simplify = FALSE) %>% dssSwapKeys
 
 sapply(names(sts), function(x){
-  if(!dir.exists(paste0('./data/',x))){
-    dir.create(paste0('./data/',x))
+  if(!dir.exists(paste0('../data/',x))){
+    dir.create(paste0('../data/',x))
   }
   sapply(names(sts[[x]]), function(y){
     print(y)
-    write(serializeJSON(sts[[x]][[y]]), paste0('./data/',x,'/',y))
+    write(serializeJSON(sts[[x]][[y]]), paste0('../data/',x,'/',y))
   })
 })
+datashield.logout(opals)
