61dfa894b1aa146e5d1c94b532f773a14d8db284
Iulian Dragan <iulian_dragan@yahoo.com>
2023-07-05 16:20:44 +0200

    tayside

--- src/oneVarStats.R
+++ src/oneVarStats.R
@@ -1,8 +1,14 @@
 oneVarStats <- function(x, nodes){
-  dssSubset(x, 'working_set', col.filter = paste0('"',x,'"'), datasources = nodes)
-  dssSubset(x, x, row.filter = paste0('complete.cases(',x,')'), datasources = nodes)
-  hist <- try(ds.histogram(paste0(x,'$',x),datasources = nodes)) %>% unlist(recursive = FALSE)
-  quants <- try(ds.quantileMean(paste0(x,'$',x), datasources = nodes))%>% unlist(recursive = FALSE)
-  datashield.rm(nodes, x)
+  f <- substr(x,1,1)
+  if(!is.na(as.numeric(f))){
+    newx <- paste0('x.',x)
+  } else {
+    newx <- x
+  }
+  dssSubset(newx, 'working_set', col.filter = paste0('"',x,'"'), datasources = nodes)
+  dssSubset(newx, newx, row.filter = paste0('complete.cases(',newx,')'), datasources = nodes)
+  hist <- try(ds.histogram(paste0(newx,'$',x),datasources = nodes)) %>% unlist(recursive = FALSE)
+  quants <- try(ds.quantileMean(paste0(newx,'$',x), datasources = nodes))%>% unlist(recursive = FALSE)
+  datashield.rm(nodes, newx)
   list(hist = hist, quants = quants)
 }
